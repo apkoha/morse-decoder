1463dc480b79febd9693a72fa6307fabf9332b07
apkoha <79258179+apkoha@users.noreply.github.com>
2024-08-10 21:40:57 +0300

    feat: function complete

--- src/index.js
+++ src/index.js
@@ -38,7 +38,54 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let letter;
+  let morseLetter;
+  let symbol;
+  let morseSymbol;
+  let result = "";
+
+  //разбиваем строку на части по 10 символов
+  for (let i = 0; i < expr.length; i+=10) {
+    let decade = expr.slice(i, i + 10);
+    
+    //меняем звёздочки на пробел
+    if (decade === "**********") {
+      letter = " ";
+    } else {
+      morseLetter = "";
+
+      //строки с десятью символами делим на строки по два символа
+      for (let j = 0; j < expr.length; j+=2) {
+        symbol = decade.slice(j, j + 2);
+        
+        //преобразовываем 11 в тире
+        morseSymbol = "";
+        if (symbol === "11") {
+          morseSymbol = "-";
+        }
+
+        //преобразовываем 10 в точку
+        if (symbol === "10") {
+          morseSymbol = ".";
+        }
+
+        //получаем из точек и тире букву в азбуке Морзе
+        morseLetter = morseLetter + morseSymbol;
+      }
+
+      //преобразуем в латинские буквы
+      for (let key in MORSE_TABLE) {
+        if (key === morseLetter) {
+          letter = MORSE_TABLE[key];
+        }
+      }
+    }
+
+    //собираем строку из латинских букв
+    result = result + letter;
+  }
+  
+  return result;
 }
 
 module.exports = {
